73c00b917c566cee60ec2e018b65e76cbc3cf7db
Glenino <55541875+Glenino@users.noreply.github.com>
2020-02-27 15:27:44 +0100

    Add files via upload

---
+++ frontend-voor-designers-1920/opdracht1/js/script.js
@@ -0,0 +1,66 @@
+/*
+  Wat doe je ook alweer in Javascript voor een micro-interactie?
+  1. Gebruik de querySelector om een element in je html te selecteren
+  2. Koppel een evenListener aan het element om een mouse-event te detecteren
+  3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
+*/
+
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function (event) {
+
+}, false);
+
+document.addEventListener("dragstart", function (event) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function (event) {
+    // reset the transparency
+    event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function (event) {
+    // prevent default to allow drop
+    event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function (event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className == "dropzone") {
+        event.target.style.background = "lightblue";
+    }
+
+}, false);
+
+document.addEventListener("dragleave", function (event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className == "dropzone") {
+        event.target.style.background = "";
+    }
+
+}, false);
+
+document.addEventListener("drop", function (event) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if (event.target.className == "dropzone") {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild(dragged);
+        event.target.appendChild(dragged);
+    }
+}, false);
+                
+                          //bron: https://developer.mozilla.org/nl/docs/Web/API/Document/drag_event//
